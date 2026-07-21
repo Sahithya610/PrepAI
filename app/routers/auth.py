bb6ed03c30f43e95ff969291e0f8ed2545b65338
Sahithya610 <sahithyakolli8@gmail.com>
2026-07-21 12:25:34 +0530

    feat: complete auth system - register, login, protected routes

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,23 +1,18 @@
 from fastapi import APIRouter
-from app.schemas.user import UserCreate, UserLogin
+from app.schemas.user import UserCreate, UserLogin, UserResponse
 from app.services.user import login_user
 from app.database import get_db
 from sqlalchemy.orm import Session
 from app.schemas.auth import Token
 from fastapi import Depends
+from app.services.user import create_user
 router = APIRouter(prefix="/auth", tags=["users"])
 
 users = []
 
-@router.post("/register", status_code=201, response_model=UserCreate)
-def register(user: UserCreate):
-    new_user = {
-        "name" : user.name,
-        "email": user.email,
-        "password":user.password
-    }
-    users.append(new_user)
-    return {"message": "authentication okay"}
+@router.post("/register", status_code=201, response_model=UserResponse)
+def register(user: UserCreate, db: Session=Depends(get_db)):
+    return create_user(db, user)
 
 @router.post("/login", status_code=200, response_model=Token)
 def login(login : UserLogin, db: Session=Depends(get_db)):
