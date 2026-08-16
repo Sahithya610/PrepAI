b5710fdda34188c0eb3b7c9f1fff46dc651b006d
Sahithya610 <sahithyakolli8@gmail.com>
2026-08-16 22:58:16 +0530

    fix: add APP_VERSION to settings

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     APP_NAME: str = "PrepAI"
+    APP_VERSION: str = "V1"
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
